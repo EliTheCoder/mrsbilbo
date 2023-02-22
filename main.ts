46f5c5b42aef3273584531050f0f53bf556a019a
EliTheCoder <ex.games.org@gmail.com>
2023-02-22 12:37:30 -0600

    moved ids to config.toml file

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { deleteMessage, sendMessage, getMember, createBot, Intents, startBot } from "https://deno.land/x/discordeno@18.0.0/mod.ts";
 import { getActualChannel, getWebhook } from "./readdata.ts";
+import { parse } from "https://deno.land/std@0.97.0/encoding/toml.ts";
 
 if (Deno.env.get("MRSBILBO_TOKEN") === undefined) throw new Error("Missing MRSBILBO_TOKEN environment variable");
 
@@ -13,17 +14,17 @@ const bot = createBot({
     },
 });
 
-const talkerRole = BigInt("1077066335466557440");
-const deepGuyId = BigInt("503611493804277780");
-const mrsBilboId = BigInt("1077013349625249855");
+
+const config = parse(await Deno.readTextFile("config.toml"));
+if (!["talkerRole", "deepGuyId", "mrsBilboId"].every(key => Object.keys(config).includes(key))) throw new Error("Missing config keys");
 
 bot.events.messageCreate = async (b, message) => {
     if (message.guildId === undefined) return;
-    if (message.guildId === mrsBilboId) {
+    if (message.guildId === BigInt(<string>config.mrsBilboId)) {
         const actualChannel = getActualChannel(message.channelId.toString());
         if (actualChannel === undefined) return;
         if (message.isFromBot) return;
-        if (!(await getMember(b, message.guildId, message.authorId)).roles.includes(talkerRole)) return;
+        if (!(await getMember(b, message.guildId, message.authorId)).roles.includes(BigInt(<string>config.talkerRole))) return;
         await deleteMessage(b, message.channelId, message.id);
         await sendMessage(b, actualChannel, {content: message.content});
         return;
@@ -38,7 +39,7 @@ bot.events.messageCreate = async (b, message) => {
         },
         body: JSON.stringify({
             username: member.nick ?? member.user?.username,
-            content: message.content.replaceAll(`<@${deepGuyId}>`, `<@${talkerRole}>`),
+            content: message.content.replaceAll(`<@${config.deepGuyId}>`, `<@${config.talkerRole}>`),
             avatar_url: getAvatarURL(member.user?.avatar, member.user?.id),
             embeds: message.attachments.map(attachment => {
                 return {image: {url: attachment.url}};
