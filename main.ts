c471968a4c071483540469500d11cc95f14fcde1
EliTheCoder <ex.games.org@gmail.com>
2023-02-22 12:53:58 -0600

    allow bots to talk for deep guy if needed

--- main.ts
+++ main.ts
@@ -32,7 +32,7 @@ bot.events.messageCreate = async (b, message) => {
 async function sendMessageToActualServer(b: Bot, message: Message) {
     const actualChannel = getActualChannel(message.channelId.toString());
     if (actualChannel === undefined) return;
-    if (message.isFromBot) return;
+    if (message.webhookId != undefined) return;
     if (!(await getMember(b, message.guildId!, message.authorId)).roles.includes(BigInt(<string>config.talkerRole))) return;
     await deleteMessage(b, message.channelId, message.id);
     await sendMessage(b, actualChannel, {content: message.content});
