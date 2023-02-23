e151797265e0a87335c370ee149d33ebe867b969
Eli Frigo <ex.games.org@gmail.com>
2023-02-23 11:14:20 -0600

    changed data filter to only allow 2 values

--- readdata.ts
+++ readdata.ts
@@ -2,7 +2,7 @@ const data = (await Deno.readTextFile("./data.ssv"))
     .split("\n")
     .filter(line => !line.startsWith("//"))
     .map(line => line.split(" "))
-    .filter(line => line.length === 3);
+    .filter(line => line.length === 2);
 
 export function getActualChannel(id: string) {
     const index = data.findIndex(element => element[0] === id);
