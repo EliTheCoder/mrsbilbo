b74418b18d092571bb71bb5232846464c1e2d98c
Eli Frigo <ex.games.org@gmail.com>
2023-02-23 02:02:23 -0600

    removed webhooks from data.ssv

--- readdata.ts
+++ readdata.ts
@@ -8,9 +8,3 @@ export function getActualChannel(id: string) {
     const index = data.findIndex(element => element[0] === id);
     return index === -1 ? undefined : data[index][1];
 }
-
-export function getWebhook(id: string) {
-    const index = data.findIndex(element => element[1] === id);
-    return index === -1 ? undefined : data[index][2];
-}
-
